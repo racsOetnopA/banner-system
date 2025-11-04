cd66d9b5ca534e79f711eaef80d762913221884a
racsoetnopa <oskr.aponte@gmail.com>
2025-11-04 19:29:27 -0400

    ajuste de login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,11 +17,6 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen bg-cover bg-center flex items-center justify-center sm:items-start sm:justify-end p-6 sm:p-12" style="background-image: url('{{ asset('storage/fondo/fonto-banners-2.png') }}');">
             <div class="w-full sm:max-w-md mx-4 sm:mx-0">
-                <div class="flex justify-end mb-4">
-                    <a href="/">
-                        <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                    </a>
-                </div>
 
                 <div class="w-full px-6 py-4 bg-white bg-opacity-80 shadow-md overflow-hidden sm:rounded-lg">
                     {{ $slot }}
